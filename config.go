280fc16521af584059d962b787d43836b46c79a5
Ahmed-Labs <ahmedelhagjr@gmail.com>
2024-06-06 01:18:29 -0400

    Add discord server list in config to specify monitored channels

---
+++ config.go
@@ -0,0 +1,27 @@
+package main
+
+// Discord Config
+
+type Server struct {
+	Name     string
+	Channels []Channel
+	Enabled  bool
+}
+
+type Channel struct {
+	Name string
+	ID   string
+}
+
+var ServerList = []Server{
+	{
+		Name:     "MechMarket",
+		Channels: []Channel{{"selling", "427630953100476436"}, {"buying", "427630933131395103"}},
+		Enabled:  true,
+	},
+	{
+		Name:     "Canadian Mechanical Keyboards",
+		Channels: []Channel{{"selling", "379790816568410112"}},
+		Enabled:  true,
+	},
+}
--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"mechfeed/channels"
 	"mechfeed/discord-portal"
 	"mechfeed/reddit-portal"
 )
 
+var DiscordConfig = make(map[string]Server)
+
+func init_config(){
+	for _, server := range ServerList {
+		for _, channel := range server.Channels {
+			DiscordConfig[channel.ID] = server
+		}
+	}
+}
 func main() {
+	init_config()
 	go discordportal.Listen()
 	go redditportal.Monitor()
 
@@ -17,16 +26,24 @@ func main() {
 		select {
 		case discord_msg := <-channels.DiscordChannel:
 			// log.Printf("Discord: %+v", discord_msg)
-			log.Printf("[ DISCORD ]: ")
-			PrettyPrint(discord_msg)
+			// log.Printf("[ DISCORD ]: ")
+			discord_handler(discord_msg)
 
 		case reddit_msg := <-channels.RedditChannel:
 			// log.Printf("Reddit: %+v", reddit_msg)
-			log.Printf("[ REDDIT ]: ")
+			// log.Printf("[ REDDIT ]: ")
 			PrettyPrint(reddit_msg)
 		}
 
 	}
+}
+
+func discord_handler(msg channels.DiscordMessage){
+	_, exists := DiscordConfig[msg.ChannelID]
+	if !exists {
+		return
+	}
+	PrettyPrint(msg)
 }
 
 func PrettyPrint(data interface{}) {
@@ -34,8 +51,8 @@ func PrettyPrint(data interface{}) {
 	//    var err := error
 	p, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
-	fmt.Printf("%s \n", p)
+	log.Printf("%s \n", p)
 }
