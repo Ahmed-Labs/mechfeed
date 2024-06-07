d484dded2c702a1c6ce4dca265fcc6365426b361
Ahmed-Labs <ahmedelhagjr@gmail.com>
2024-06-07 15:03:48 -0400

    Add discord and reddit handlers as a placeholder, and some refactoring

--- main.go
+++ main.go
@@ -2,53 +2,76 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"mechfeed/channels"
 	"mechfeed/discord-portal"
 	"mechfeed/reddit-portal"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
-var DiscordConfig = make(map[string]Server)
+var DISCORD_CHANNELS = make(map[string]Channel) // Discord channels indexed by channel ID
+var DISCORD_SERVERS = make(map[string]Server) // Discord servers indexed by channel ID
+var DISCORD_WEBHOOK_URL string
 
-func init_config(){
+func load_config() error{
+
+	godotenv.Load()
+	DISCORD_WEBHOOK_URL = os.Getenv("DISCORD_WEBHOOK")
+	if DISCORD_WEBHOOK_URL == "" {
+		return errors.New("no discord weebhook found")
+	}
+
 	for _, server := range ServerList {
 		for _, channel := range server.Channels {
-			DiscordConfig[channel.ID] = server
+			DISCORD_CHANNELS[channel.ID] = channel
+			DISCORD_SERVERS[channel.ID] = server
 		}
 	}
+	return nil
 }
+
 func main() {
-	init_config()
+	if err := load_config(); err != nil {
+		log.Fatal(err)
+	}
+
 	go discordportal.Listen()
 	go redditportal.Monitor()
 
 	for {
 		select {
 		case discord_msg := <-channels.DiscordChannel:
-			// log.Printf("Discord: %+v", discord_msg)
-			// log.Printf("[ DISCORD ]: ")
-			discord_handler(discord_msg)
+			go discord_handler(discord_msg)
 
 		case reddit_msg := <-channels.RedditChannel:
-			// log.Printf("Reddit: %+v", reddit_msg)
-			// log.Printf("[ REDDIT ]: ")
-			PrettyPrint(reddit_msg)
+			go reddit_handler(reddit_msg)
 		}
 
 	}
 }
 
-func discord_handler(msg channels.DiscordMessage){
-	_, exists := DiscordConfig[msg.ChannelID]
-	if !exists {
-		return
+func discord_handler(msg channels.DiscordMessage) error {
+	msg_channel, ok := DISCORD_CHANNELS[msg.ChannelID]
+	if !ok {
+		return nil // Channel not being monitored
 	}
+	msg_server := DISCORD_SERVERS[msg.ChannelID]
+
+	log.Printf("Server: %s, Channel: %s ", msg_server.Name, msg_channel.Name)
+	PrettyPrint(msg)
+	return nil
+}
+
+func reddit_handler(msg channels.RedditMessage) {
 	PrettyPrint(msg)
 }
 
 func PrettyPrint(data interface{}) {
 	var p []byte
-	//    var err := error
+
 	p, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		log.Println(err)
