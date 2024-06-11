f7f0d39abb2810020c9addf64f45d529a6e9230f
Ahmed-Labs <ahmedelhagjr@gmail.com>
2024-06-10 21:51:27 -0400

    Update webhook notifications to support both discord and reddit alerts

--- notifications/notifications.go
+++ notifications/notifications.go
@@ -3,54 +3,73 @@ package notifications
 import (
 	"bytes"
 	"encoding/json"
+	"mechfeed/channels"
 	"net/http"
 	"time"
 )
 
-
-
-func SendWebhook(webhookURL string, message DiscordNoti) error {
-
-	jsonPayload, err := json.Marshal(message)
+func SendWebhook(webhookURL string, message interface{}) error {
+	json_payload, err := json.Marshal(message)
 	if err != nil {
 		return err
 	}
 
-	req, err := http.NewRequest("POST", webhookURL, bytes.NewBuffer(jsonPayload))
+	req, err := http.NewRequest("POST", webhookURL, bytes.NewBuffer(json_payload))
 	if err != nil {
 		return err
 	}
 
 	req.Header.Set("Content-Type", "application/json")
-
 	client := http.Client{}
 	resp, err := client.Do(req)
+
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close()
 
+	resp.Body.Close()
 	return nil
 }
 
-func CreateNotification(postData ProcessedRedditPost) DiscordNoti {
+func CreateNotificationReddit(data channels.RedditMessage) DiscordNoti {
 	return DiscordNoti{
 		Content: nil,
 		Embeds: []Embed{
 			{
-				Title:     postData.Title,
-				URL:       postData.URL,
-				Color:     16734296,
+				Title: data.Title,
+				URL:   data.URL,
+				Color: 16734296,
 				Fields: []Field{
-					{Name: "Posted by", Value: "u/"+postData.Author+" [[PM]](https://www.reddit.com/message/compose/?to=" + postData.Author + ")", Inline: true},
-					{Name: "Category", Value: postData.Category, Inline: true},
-					{Name: "Imgur Link", Value: postData.Imgur},
+					{Name: "Posted by", Value: "u/" + data.Author + " [[PM]](https://www.reddit.com/message/compose/?to=" + data.Author + ")", Inline: true},
+					{Name: "Category", Value: data.Category, Inline: true},
+					{Name: "Imgur Link", Value: data.Imgur},
 				},
 				Footer:    Footer{Text: "mechfeed"},
 				Timestamp: time.Now().UTC().Format("2006-01-02T15:04:05.000Z"),
-				Image:     Image{URL: postData.Thumbnail},
+				Image:     Image{URL: data.Thumbnail},
 			},
 		},
-		Username:   "mechfeed",
+		Username: "mechfeed",
+	}
+}
+
+func CreateNotificationDiscord(server, channl, alert string, data channels.DiscordMessage) DiscordNoti {
+	return DiscordNoti{
+		Content:  nil,
+		Embeds: []Embed{
+			{
+				Color: 5727730,
+				Fields: []Field{
+					{Name: "Server", Value: server, Inline: true},
+					{Name: "Channel", Value: channl, Inline: true},
+					{Name: "Sent by", Value: "<@!" + data.Author.ID + ">", Inline: true},
+					{Name: "Matched alert", Value: alert, Inline: true},
+					{Name: "Message", Value: data.Content},
+				},
+				Footer:    Footer{Text: "mechfeed"},
+				Timestamp: time.Now().UTC().Format("2006-01-02T15:04:05.000Z"),
+			},
+		},
+		Username: "mechfeed",
 	}
 }
