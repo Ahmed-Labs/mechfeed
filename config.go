4b9868bcaef1c43648f2c2c3b917624ab1b75b66
Ahmed-Labs <ahmedelhagjr@gmail.com>
2024-09-19 21:16:24 -0400

    Add suggested discord servers and channels to monitored list

--- config.go
+++ config.go
@@ -5,7 +5,6 @@ package main
 type Server struct {
 	Name     string
 	Channels []Channel
-	Enabled  bool
 }
 
 type Channel struct {
@@ -15,18 +14,51 @@ type Channel struct {
 
 var ServerList = []Server{
 	{
-		Name:     "MechMarket",
-		Channels: []Channel{{"selling", "427630953100476436"}, {"buying", "427630933131395103"}},
-		Enabled:  true,
+		Name: "MechMarket",
+		Channels: []Channel{
+			{"selling", "427630953100476436"},
+			{"buying", "427630933131395103"},
+			{"selling-artisans", "539626107176091648"},
+			{"buying-artisans", "539626087303348254"},
+			{"gb-spot-selling-trading", "507695814223724546"},
+		},
 	},
 	{
-		Name:     "Canadian Mechanical Keyboards",
-		Channels: []Channel{{"selling", "379790816568410112"}},
-		Enabled:  true,
+		Name: "Canadian Mechanical Keyboards",
+		Channels: []Channel{
+			{"market", "379790816568410112"},
+		},
+	},
+	{
+		Name: "MechGroupBuys",
+		Channels: []Channel{
+			{"sell", "683068530513674240"},
+			{"buy", "683068253257596939"},
+		},
+	},
+	{
+		Name: "Deskhero.ca",
+		Channels: []Channel{
+			{"market", "689578698436771858"},
+		},
+	},
+	{
+		Name: "Top Clack",
+		Channels: []Channel{
+			{"mechmarket", "371096775467073536"},
+		},
+	},
+	{
+		Name: "Artisan Trading",
+		Channels: []Channel{
+			{"general-selling", "783779301328683028"},
+			{"general-buying", "783779330315911190"},
+			{"general-trading", "783777689063653396"},
+			{"metal-novelties", "788549951428100136"},
+		},
 	},
 	// {
 	// 	Name:     "Mechfeed",
 	// 	Channels: []Channel{{"mechfeed", "968791988465983518"}},
-	// 	Enabled:  true,
 	// },
 }
