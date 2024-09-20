306bb014d3f6e01186d9b029b03deda111db89bc
Ahmed-Labs <ahmedelhagjr@gmail.com>
2024-09-19 21:27:05 -0400

    Refactor main application to simplify discord and reddit message handlers

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"mechfeed/channels"
 	"mechfeed/discord-portal"
@@ -45,30 +44,32 @@ func load_config() error {
 	return nil
 }
 
-
 func main() {
+	// Load env
 	if err := load_config(); err != nil {
 		log.Fatal(err)
 	}
 
+	// Get user DB connection
 	repo, err := users.DBConnection()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer repo.Db.Close()
 
+	// Mechfeed client discord bot
 	go bot.MechfeedBot()
+
+	// Wrapped goroutines for Discord & Reddit monitors 
 	go portal_supervisor(discordportal.Listen, "discordportal", time.Millisecond*300)
 	go portal_supervisor(redditportal.Monitor, "redditportal", time.Millisecond*300)
 
 	for {
 		select {
 		case discord_msg := <-channels.DiscordChannel:
-			log.Println(discord_msg)
 			go discord_handler(repo, discord_msg)
 
 		case reddit_msg := <-channels.RedditChannel:
-			log.Println(reddit_msg)
 			go reddit_handler(repo, reddit_msg)
 		}
 	}
@@ -89,11 +90,10 @@ func portal_supervisor(portal func(), name string, restartDelay time.Duration) {
 }
 
 func discord_handler(r *users.Repository, msg channels.DiscordMessage) {
-	msg_channel, ok := DISCORD_CHANNELS[msg.ChannelID]
+	_, ok := DISCORD_CHANNELS[msg.ChannelID]
 	if !ok {
 		return // Channel not being monitored
 	}
-	msg_server := DISCORD_SERVERS[msg.ChannelID]
 
 	alerts, err := r.Queries.GetAlerts(r.Ctx)
 	if err != nil {
@@ -101,47 +101,14 @@ func discord_handler(r *users.Repository, msg channels.DiscordMessage) {
 		return
 	}
 	
-	// log.Printf("Server: %s, Channel: %s ", msg_server.Name, msg_channel.Name)
-	for _, a := range alerts {
-		go func(alert users.UserAlert) {
-			user, err := r.Queries.GetUser(r.Ctx, alert.ID)
-			if err != nil {
-				log.Println("failed to fetch user: ", alert.ID, " , error: ", err)
-				return
-			}
-
-			if !filter.FilterKeywords(msg.Content, alert.Keyword) {
-				return
-			}
-
-			for _, u := range alert.Ignored {
-				if u == msg.Author.Username {
-					log.Printf("Skipping alert... '%s' is ignored by %s", u, user.Username)
-					return
-				}
-			}
-
-			log.Println("Sending Discord notification via DM to user:", user.Username, "Keyword:", alert.Keyword, "Message:", msg)
-			bot.IsolatedSendEmbedDM(
-				user.ID, 
-				notifications.CreateDiscordNotificationMessageEmbed(msg_server.Name, msg_channel.Name, alert.Keyword, msg),
-			)
-			if user.WebhookUrl.Valid {
-				log.Println("Notifying user through webhook:", user.WebhookUrl)
-				notifications.SendWebhook(
-					user.WebhookUrl.String, 
-					notifications.CreateNotificationDiscord(
-						msg_server.Name, msg_channel.Name, alert.Keyword, msg,
-					),
-				)
-			} else {
-				log.Println("user did not set Webhook URL.")
-			}
-				
-		}(a)
+	for _, alert := range alerts {
+		// Notify user if alert matches
+		if filter.FilterKeywords(msg.Content, alert.Keyword) {
+			go discord_notify(r, msg, alert)
+		}
+		
 	}
 }
-
 
 func reddit_handler(r *users.Repository, msg channels.RedditMessage) {
 	// Notify public mechmarket channel
@@ -154,45 +121,79 @@ func reddit_handler(r *users.Repository, msg channels.RedditMessage) {
 		return
 	}
 
-	for _, a := range alerts {
-		go func(alert users.UserAlert) {
-			user, err := r.Queries.GetUser(r.Ctx, alert.ID)
-			if err != nil {
-				log.Println("failed to fetch user: ", alert.ID, " , error: ", err)
-				return
-			}
-			if !filter.FilterKeywords(msg.Content, alert.Keyword) {
-				return
-			}
-
-			for _, u := range alert.Ignored {
-				if u == msg.Author {
-					log.Printf("Skipping alert... '%s' is ignored by %s", u, user.Username)
-					return
-				}
-			}
-			log.Println("Sending Reddit notification via DM to user:", user.Username, "Keyword:", alert.Keyword, "Message:", msg)
-			bot.IsolatedSendEmbedDM(
-				user.ID, 
-				notifications.CreateRedditNotificationMessageEmbed(msg, alert.Keyword),
-			)
-			if user.WebhookUrl.Valid {
-				log.Println("Notifying user through webhook: ", user.WebhookUrl)
-				notifications.SendWebhook(user.WebhookUrl.String, notifications.CreateNotificationReddit(msg))
-			} else {
-				log.Println("Webhook URL invalid: ", user.WebhookUrl)
-			}
-		}(a)
+	for _, alert := range alerts {
+		// Notify user if alert matches
+		if filter.FilterKeywords(msg.Content, alert.Keyword) {
+			go reddit_notify(r, msg, alert)
+		}
 	}
 }
 
-func PrettyPrint(data interface{}) {
-	var p []byte
+func discord_notify(r *users.Repository, msg channels.DiscordMessage, alert users.UserAlert) {
+	msg_server := DISCORD_SERVERS[msg.ChannelID]
+	msg_channel := DISCORD_CHANNELS[msg.ChannelID]
 
-	p, err := json.MarshalIndent(data, "", "\t")
+	// Get user that set alert
+	user, err := r.Queries.GetUser(r.Ctx, alert.ID)
 	if err != nil {
-		log.Println(err)
+		log.Println("failed to fetch user: ", alert.ID, " , error: ", err)
 		return
 	}
-	log.Printf("%s \n", p)
+
+	// Skip alert if message author is ignored
+	for _, u := range alert.Ignored {
+		if u == msg.Author.Username {
+			log.Printf("Skipping alert... '%s' is ignored by %s", u, user.Username)
+			return
+		}
+	}
+
+	// Send DM notification
+	log.Println("Sending Discord notification via DM to user:", user.Username, "Keyword:", alert.Keyword, "Message:", msg)
+	bot.IsolatedSendEmbedDM(
+		user.ID, 
+		notifications.CreateDiscordNotificationMessageEmbed(msg_server.Name, msg_channel.Name, alert.Keyword, msg),
+	)
+
+	// Send webhook notification if user opted in
+	if user.WebhookUrl.Valid {
+		log.Println("Notifying user through webhook:", user.WebhookUrl)
+		notifications.SendWebhook(
+			user.WebhookUrl.String, 
+			notifications.CreateNotificationDiscord(
+				msg_server.Name, msg_channel.Name, alert.Keyword, msg,
+			),
+		)
+	}
+}
+
+
+func reddit_notify(r *users.Repository, msg channels.RedditMessage, alert users.UserAlert) {
+	// Get user that set alert
+	user, err := r.Queries.GetUser(r.Ctx, alert.ID)
+	if err != nil {
+		log.Println("failed to fetch user: ", alert.ID, " , error: ", err)
+		return
+	}
+
+	// Skip alert if message author is ignored
+	for _, u := range alert.Ignored {
+		if u == msg.Author {
+			log.Printf("Skipping alert... '%s' is ignored by %s", u, user.Username)
+			return
+		}
+	}
+
+	// Send DM notification
+	log.Println("Sending Reddit notification via DM to user:", user.Username, "Keyword:", alert.Keyword, "Message:", msg)
+	bot.IsolatedSendEmbedDM(
+		user.ID, 
+		notifications.CreateRedditNotificationMessageEmbed(msg, alert.Keyword),
+	)
+
+	// Send webhook notification if user opted in
+	if user.WebhookUrl.Valid {
+		log.Println("Notifying user through webhook: ", user.WebhookUrl)
+		notifications.SendWebhook(user.WebhookUrl.String, notifications.CreateNotificationReddit(msg))
+	}
 }
